078e775bb198e08a93f8653b5ada1f3be81b4765
sada gopan <sada.gopan@gmail.com>
2024-02-02 15:52:15 +0530

    java program

---
+++ Test.java
@@ -0,0 +1,16 @@
+
+class Test{
+
+  public static void main(String[] args){
+     int a = 10;
+     int b = 20;
+ 
+     if(a>b){
+      System.out.println("a is big number");
+     }else{
+       System.out.println("b is big number");
+     }
+    
+  }
+
+}
